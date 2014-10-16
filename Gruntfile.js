349365b0da045441ff651dea92c0325df4d426d4
Mats Lundström <mats.a.lundstrom@gmail.com>
2014-10-16 22:01:06 +0200

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc',
+          reporter: require('jshint-stylish')
+        },
+        src: 'Gruntfile.js'
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', []);
+};
